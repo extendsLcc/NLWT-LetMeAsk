cf5ec84441b0f8f10ae583c7a024c4d02d73f535
extendslcc <lucas.lcc@hotmail.com>
2021-06-22 22:17:22 -0300

    :truck: RENAMING: changed style.scss to styles.scss for better semantic naming

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import illustrationImg from '../../assets/images/illustration.svg'
 import logoImg from '../../assets/images/logo.svg'
 import googleIconImg from '../../assets/images/google-icon.svg'
 
-import './style.scss';
+import './styles.scss';
 
 import { Button } from "../../components/Button/Button";
 
