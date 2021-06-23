60e9236ca9e3d56d364faa31515ed45f1407a7f0
extendslcc <lucas.lcc@hotmail.com>
2021-06-22 22:33:07 -0300

    :recycle: REFACTOR: app to use react-router-dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
 import { Home } from "./pages/Home/Home";
+import { NewRom } from "./pages/NewRom/NewRom";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRom} />
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,20 @@
-import illustrationImg from '../../assets/images/illustration.svg'
-import logoImg from '../../assets/images/logo.svg'
-import googleIconImg from '../../assets/images/google-icon.svg'
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import googleIconImg from '../../assets/images/google-icon.svg';
 
 import './styles.scss';
 
 import { Button } from "../../components/Button/Button";
 
 export function Home() {
+  const history = useHistory();
+
+  function navigatoToNewRoom() {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +25,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="LetMeAsk" />
-          <button className='create-room'>
+          <button className='create-room' onClick={navigatoToNewRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             <span>Crie sua sala com o Google</span>
           </button>
