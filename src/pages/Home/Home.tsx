fd272cfe272283c48d1494610cae1c91a7022746
extendslcc <lucas.lcc@hotmail.com>
2021-06-22 22:07:11 -0300

    :desktop_computer: UI: created styled interface of the home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,36 @@
+import illustrationImg from '../../assets/images/illustration.svg'
+import logoImg from '../../assets/images/logo.svg'
+import googleIconImg from '../../assets/images/google-icon.svg'
+
+import './style.scss';
+
+import { Button } from "../../components/Button/Button";
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as duvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="LetMeAsk" />
+          <button className='create-room'>
+            <img src={googleIconImg} alt="Logo do Google" />
+            <span>Crie sua sala com o Google</span>
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
