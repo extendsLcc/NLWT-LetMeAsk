738e9c7051b33d13e4eb2d84dee593dc912f5028
extendslcc <lucas.lcc@hotmail.com>
2021-06-24 00:17:01 -0300

    :desktop_computer: UI: created base ui of room page

---
+++ src/pages/Room/Room.tsx
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom";
+
+import logoImg from '../../assets/images/logo.svg';
+import { Button } from "../../components/Button/Button";
+import { RoomCode } from "../../components/RoomCode/RoomCode";
+
+import './styles.scss';
+
+type RoomParams = {
+  id: string;
+}
+
+export function Room() {
+
+  const params = useParams<RoomParams>();
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="LetMeAsk" />
+          <RoomCode code={params.id} />
+        </div>
+      </header>
+      <main>
+
+        <div className="room-title">
+          <h1>Sala</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+            <textarea
+              placeholder="Oque você quer perguntar?"
+            />
+
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+
+      </main>
+    </div>
+  );
+}
