5e9c6c0784b53fa4cbd5d276c49c55b728f1f697
extendslcc <lucas.lcc@hotmail.com>
2021-06-24 00:18:43 -0300

    :star: FEAT: added navigation to Room page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,10 +8,13 @@ import googleIconImg from '../../assets/images/google-icon.svg';
 import { Button } from "../../components/Button/Button";
 
 import './styles.scss';
+import { FormEvent, useState } from "react";
+import { database } from "../../services/firebase";
 
 export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRom() {
 
@@ -20,6 +23,25 @@ export function Home() {
     }
 
     history.push('/rooms/new');
+
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+      return ;
+    }
+
+    history.push(`/rooms/${roomCode}`)
 
   }
 
@@ -38,10 +60,12 @@ export function Home() {
             <span>Crie sua sala com o Google</span>
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
--- src/pages/NewRoom/NewRoom.tsx
+++ src/pages/NewRoom/NewRoom.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import illustrationImg from '../../assets/images/illustration.svg';
 import logoImg from '../../assets/images/logo.svg';
@@ -14,6 +14,7 @@ import { useAuth } from "../../hooks/useAuth";
 export function NewRoom() {
 
   const { user } = useAuth();
+  const history = useHistory();
   const [newRoom, setNewRoom] = useState('');
 
   async function handleCreateRoom(event: FormEvent) {
@@ -30,6 +31,8 @@ export function NewRoom() {
       title: newRoom,
       authorId: user?.id
     });
+
+    history.push(`/rooms/${firebaseRoom.key}`);
 
   }
 
